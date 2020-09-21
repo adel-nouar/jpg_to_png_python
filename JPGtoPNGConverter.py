94311f62a108cefa31b5032529b8cfe9963fdda8
adel <adel@nc-ass-vip.sdv.fr>
2020-09-21 13:04:20 +0200

    adding starting project

---
+++ JPGtoPNGConverter.py
@@ -0,0 +1,13 @@
+import sys
+import os
+from PIL import Image
+
+# 1- Grab first and second argument
+
+# 2- Check if arg2 directory exist, if not create this directory
+
+# 3- Loop through arg1 directory
+
+# 4- Convert images to png
+
+# 5 - Save to the new folder
